14f3a8706553ee4c784c7084e764966f6364f4de
RohitRathore25 <singhrathore00008@gmail.com>
2026-01-20 12:12:01 +0530

    final push

--- app.py
+++ app.py
@@ -36,29 +36,30 @@ st.write(tips)
 job_desc = st.text_area(':blue[Copy and Paste your job description over here.]', max_chars=5000)
 
 if st.button('Submit'):
-    prompt = f'''
-    <Role> you are an expert in analyzing resume and matching it with the job description
-    <Goal> Match your resume and the job descriptin provided by the applicant and create a report
-    <Context> The following content has been provided by the applicant
-    * Resume : {file_text}
-    * Job Description: {job_desc}
-    <Format> The report should follow these steps:
-    * Give a brief description of the applicant in 3 to 5 lines.
-    * Describe in percentage what are the chances of this resume of getting selected.
-    * Need not be the exact percentage, you can give interval of percentage.
-    * Give the expected ATS Score along with matching and non matching keywords.
-    * Perform SWAT Anaalysis and explain each parameter i.e. strength, Weekness, Oppurinity and threat.
-    * Give what all the section in the current resume that are required to be change in order to improve the ATS score and selection percentage
-    * Show both current version and improve version of the section in resume.
-    * Create two samples resume which can maximize the ATS score and selection percentage.
+    with st.spinner('Processing...'):        
+        prompt = f'''
+        <Role> you are an expert in analyzing resume and matching it with the job description
+        <Goal> Match your resume and the job descriptin provided by the applicant and create a report
+        <Context> The following content has been provided by the applicant
+        * Resume : {file_text}
+        * Job Description: {job_desc}
+        <Format> The report should follow these steps:
+        * Give a brief description of the applicant in 3 to 5 lines.
+        * Describe in percentage what are the chances of this resume of getting selected.
+        * Need not be the exact percentage, you can give interval of percentage.
+        * Give the expected ATS Score along with matching and non matching keywords.
+        * Perform SWAT Anaalysis and explain each parameter i.e. strength, Weekness, Oppurinity and threat.
+        * Give what all the section in the current resume that are required to be change in order to improve the ATS score and selection percentage
+        * Show both current version and improve version of the section in resume.
+        * Create two samples resume which can maximize the ATS score and selection percentage.
 
-    <Instruction>
-    * Use bullets for explanation wherever possible.
-    * Create tables for description wherever required.
-    * Strictly do not add any new skill in sample resume.
-    * The format of sample resumes should be in such a way that they can be copied and pasted directly in word.
-    '''
+        <Instruction>
+        * Use bullets for explanation wherever possible.
+        * Create tables for description wherever required.
+        * Strictly do not add any new skill in sample resume.
+        * The format of sample resumes should be in such a way that they can be copied and pasted directly in word.
+        '''
 
 
-    response = model.invoke(prompt)
-    st.write(response.content)
+        response = model.invoke(prompt)
+        st.write(response.content)
